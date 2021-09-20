7a5786036628880d5633792eefad5f99383d5c60
Jon Fleming <jon.fleming@hotmail.com>
2021-09-19 19:59:46 -0700

    Updates for Azure FHIR API

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const express = require('express'),
   shell = require('shelljs'),
 
    // Modify the folder path in which responses need to be stored
-  folderPath = './Responses/',
+  root = './FHIR/',
   defaultFileExtension = 'json', // Change the default file extension
   bodyParser = require('body-parser'),
   DEFAULT_MODE = 'writeFile',
   path = require('path');
 
 // Create the folder path in case it doesn't exist
-shell.mkdir('-p', folderPath);
+shell.mkdir('-p', root);
 
  // Change the limits according to your response size
 app.use(bodyParser.json({limit: '50mb', extended: true}));
@@ -20,25 +20,25 @@ app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.get('/', (req, res) => res.send('Hello, I write data to file. Send them requests!'));
 
 app.post('/write', (req, res) => {
-  let extension = req.body.fileExtension || defaultFileExtension,
-    fsMode = req.body.mode || DEFAULT_MODE,
-    uniqueIdentifier = req.body.uniqueIdentifier ? typeof req.body.uniqueIdentifier === 'boolean' ? Date.now() : req.body.uniqueIdentifier : false,
-    filename = `${req.body.requestName}${uniqueIdentifier || ''}`,
-    filePath = `${path.join(folderPath, filename)}.${extension}`,
-    options = req.body.options || undefined;
+  let fsMode = req.body.mode || DEFAULT_MODE,
+    filename = req.body.filename,
+    folder = req.body.folder || '',
+    filePath = path.join(root, folder, filename),
+    data = typeof req.body.responseData === 'object' ? JSON.stringify(req.body.responseData, null, 4) : req.body.responseData
 
-  fs[fsMode](filePath, req.body.responseData, options, (err) => {
+  fs[fsMode](filePath, data, 'utf8', (err) => {
     if (err) {
       console.log(err);
       res.send('Error');
     }
     else {
       res.send('Success');
+      console.log(`${fsMode} - ${folder} ${filename}`);
     }
   });
 });
 
 app.listen(3000, () => {
-  console.log('ResponsesToFile App is listening now! Send them requests my way!');
-  console.log(`Data is being stored at location: ${path.join(process.cwd(), folderPath)}`);
+  console.log('postman2file is listening on port 3000');
+  console.log(`Data is being stored at location: ${path.join(process.cwd(), root)}`);
 });
